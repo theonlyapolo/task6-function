269292b495ca419f5542bdb6035e9fae982548e1
Apolo <paolacrisnvs@gmail.com>
2025-05-11 13:31:24 -0300

    Add project files.

---
+++ task6-function/Program.cs
@@ -0,0 +1,15 @@
+﻿static string Info(string nome, int idade)
+{
+    if (idade < 18)
+    {
+        Console.WriteLine("Você é menor de idade");
+    }
+    else
+    {
+        Console.WriteLine("Você é maior de idade");
+    }
+        return $"Seu nome é {nome} e tem {idade} anos.";
+}
+
+string resultado = Info("Apolo",16);
+Console.WriteLine(resultado);
